d688f31a02f93222eb11e0470681cc7fb3b11145
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-07 12:35:22 -0300

    start CountryDetailView

---
+++ CovidStats/CovidStats/ViewModel/CountryDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CountryDetailViewModel.swift
+//  CovidStats
+//
+//  Created by Grazi  Berti on 07/11/22.
+//
+
+import Foundation
+
+final class CountryDetailViewModel: ObservableObject {
+    
+}
---
+++ CovidStats/CovidStats/Views/CountryDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  CountryDetailView.swift
+//  CovidStats
+//
+//  Created by Grazi  Berti on 07/11/22.
+//
+
+import SwiftUI
+
+struct CountryDetailView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryDetailView()
+    }
+}
